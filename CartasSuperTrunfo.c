294ed46983d0bb78a884ffbcc321691c9ced8fc8
Carlawe <freitaswalle05@gmail.com>
2025-06-16 21:20:54 +0000

    ifelse

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,9 +11,20 @@ int main() {
      int pontoturistico1, pontoturistico2 ; 
      float densidadepopulacional1, densidadepopulacional2;
      float pibpercapta1, pibpercapta2;
-
-
-      printf("Bem vindo ao super trunfo ***Países***!!!!!\n");
+     float superpoder1, superpoder2;
+     
+    // densidadepopulacional1 = populacao1 / area1;
+     //pibpercapta1 = pib1 / populacao1;
+     //superpoder1 = populacao1 + area1 + pib1 + pontoturistico1 + densidadepopulacional1 + pibpercapta1;
+     
+     //densidadepopulacional2 = populacao2 / area2;
+     //pibpercapta2 = pib2 / populacao2;
+    // superpoder2 = populacao2 + area2 + pib2 + pontoturistico2 + densidadepopulacional2 + pibpercapta2;
+      
+     
+     
+     
+     printf("Bem vindo ao super trunfo ***Países***!!!!!\n");
       printf("Faça o cadastro da carta 1: \n");
      
       //Leitura dos dados da primeira carta.
@@ -45,8 +56,10 @@ int main() {
      //Saída dos dados da primeira carta.
       printf("Carta 1\n");
       printf("Estado: %s\nCódigo da cidade:%s\nNome da cidade:%s\nPopulação:%d\n", estado1, codigocidade1, nomecidade1, populacao1);
-      printf("Área: %f km2\nPIB: %f\nNumero de pontos turísticos: %d\nDensidade populacional:%.2f\nPib per capta:%.2f\n ", area1, pib1, pontoturistico1, (populacao1 / area1), (pib1 / populacao1));
-     
+      printf("Área: %f km2\nPIB: %f\nNumero de pontos turísticos: %d\n", area1, pib1, pontoturistico1);
+      printf("Densidade populacional:%.2f\n", (float)populacao1 / area1);
+      printf("Pib percapta:%.2f\n", pib1 / (float)populacao1);
+      printf("Superpoder: %f\n", (float) populacao1 + area1 + pib1 + pibpercapta1 + densidadepopulacional1 +  (float)pontoturistico1);
     
       printf("\n");
 
@@ -76,13 +89,62 @@ int main() {
       printf("\n"); // espaco entre uma sequência e outra.
       
 
-     //Saída dos dados da primeira carta.
+     //Saída dos dados da segunda carta.
      
 
       printf("Carta 2\n");
       printf("Estado: %s\nCódigo da cidade:%s\nNome da cidade:%s\nPopulação:%d\n", estado2, codigocidade2, nomecidade2, populacao2);
-      printf("Área: %f km2\nPIB: %f\nNumero de pontos turísticos: %d\nDensidadepopulacional:%.2f\nRenda per capta:%.2f\n ", area2, pib2, pontoturistico2, (populacao2 / area2), (pib2 / populacao2));
-     
+      printf("Área: %f km2\nPIB: %f\nNumero de pontos turísticos: %d\n", area2, pib2, pontoturistico2);
+      printf("Densidade populacional:%.2f\n", (float)populacao2 / area2);
+      printf("Pib percapta:%.2f\n", pib2 / (float)populacao2);
+      printf("Superpoder: %f\n", (float) populacao2 + area2 + pib2 + pibpercapta2 + densidadepopulacional2 +  (float)pontoturistico2);
+      
+       printf("\n");
+       
+      if(populacao1 > populacao2){
+        printf("População: A carta 1 venceu! (%d) \n", populacao1 >  populacao2);
+      }else{
+        printf("População: A carta 2 venceu! (%d)\n", populacao1 > populacao2);
+
+        }
+         if(area1 > area2){
+        printf("Área: A carta 1 venceu! (%d) \n", area1 > area2);
+      }else{
+          printf("Área: A carta 2 venceu! (%d)\n", area1 > area2);
+
+        }
+       if(pib1 > pib2){
+        printf("Pib: A carta 1 venceu! (%d) \n", pib1 > pib2);
+      }else{
+        printf("Pib: A carta 2 venceu! (%d)\n", pib1 > pib2);
+
+        }
+       if(pontoturistico1 > pontoturistico2){
+        printf("Número de pontos turisticos: A carta 1 venceu! (%d) \n", pontoturistico1 > pontoturistico2);
+      }else{
+        printf("Número de pontos turisticos: A carta 2 venceu! (%d)\n", pontoturistico1 > pontoturistico2);
+
+        }
+       if(densidadepopulacional1 < densidadepopulacional2){
+        printf("Densidade populacional: A carta 1 venceu! (%d) \n", densidadepopulacional1 < densidadepopulacional2);
+      }else{
+        printf("Densidade populacional: A carta 2 venceu! (%d)\n", densidadepopulacional1 < densidadepopulacional2);
+
+        }
+       if(pibpercapta1 > pibpercapta2){
+        printf("Pib percapta: A carta 1 venceu! (%d) \n", pibpercapta1 > pibpercapta2);
+      }else{
+        printf("Pib per capta: A carta 2 venceu! (%d)\n", pibpercapta1 > pibpercapta2);
+
+        }
+      
+       if(superpoder1 > superpoder2){
+        printf("Superpoder: A carta 1 venceu! (%d) \n", superpoder1 > superpoder2);
+      }else{
+        printf("Superpoder: A carta 2 venceu! (%d)\n", superpoder1 > superpoder2);
+
+        }
+        printf("\n");
 
      return 0;
 }
